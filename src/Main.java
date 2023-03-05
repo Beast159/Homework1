61c199113273c6fdd58c807d2e4e3042e73621d2
Алексей <Beast159@mail.ru>
2023-03-05 19:26:34 +0500

    Победил!!! но у меня код короче получился чем в разборе ДЗ

--- src/Main.java
+++ src/Main.java
@@ -261,9 +261,111 @@ public class Main {
             if (two > one) {
                 System.out.println("Максимальное число " + two);
             } else {
-                    System.out.println("Максимальное число " + one);
+                System.out.println("Максимальное число " + one);
+            }
+        }
+
+
+
+
+        // условный опертор 2 Задание 1
+        int clientOS = 1;
+        if (clientOS < 1) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android  по ссылке");
+        }
+
+
+        //  Задание 2
+        int clientOS1 = 0;
+        int clientDeviceYear = 2014;
+        if (clientOS1 == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            if (clientOS1 == 0 && clientDeviceYear >= 2015) {
+                System.out.println("Установите обычную версию приложения для iOS  по ссылке");
+            }
+
+            if (clientOS1 == 1 && clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android  по ссылке");
+            } else {
+                if (clientOS1 == 1 && clientDeviceYear >= 2015) {
+                    System.out.println("Установите обычную версию приложения для Android  по ссылке");
                 }
             }
         }
+
+
+        //  Задание3
+        int year = 2024;
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+
+
+        // Задание 4
+        int deliveryDistance = 95;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуются сутки для доставки");
+        } else {
+            if (deliveryDistance > 21 && deliveryDistance < 60) {
+                System.out.println("Потребуется двое суток для доставки");
+            } else {
+                if (deliveryDistance > 61 && deliveryDistance < 100) {
+                    System.out.println("Потребуется трое суток для доставки");
+                } else {
+                    System.out.println("Досавки нет");
+                }
+            }
+        }
+
+
+        // Задание 5
+        int monthNumber  = 13;
+        switch (monthNumber ) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+        }
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
